60567ecc49ded8a264d894c4f2dd07cb5b17f7da
BJC <bencochrane1@me.com>
2013-08-27 22:23:35 +1000

    previous assessments redirected

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
 	def gender_options
 		[['Male', 'Male'],['Female', 'Female']]
 	end
+
+
+	def business_unit_options
+		[['HR', 'HR'],['IT', 'IT'],['Fixed Interest', 'Fixed Interest'],['Business Banking','Business Banking'],['Personal Banking','Personal Banking'],['Private Wealth','Private Wealth']]
+	end
+
 end
